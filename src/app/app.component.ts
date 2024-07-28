56009a7e859db9d3a74538671e61f3a9c0efd422
Tomas Rivero <ttomasrivero@gmail.com>
2024-07-28 16:24:43 -0300

    Hola mundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,99 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ml-share';
+
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
+
+  private apiUrl = '/api/product-fe-recommendations/recommendations?site_id=MLA&client=pdp_comparator';
+  productLink = '';
+  productList: any[] = [];
+
+  ngOnInit() {
+    this.loadProductsFromUrl();
+  }
+
+  addProductLink() {
+    try {
+      let productId = this.extractProductId(this.productLink);
+      this.http.get(`${this.apiUrl}&product_id=${productId}`).subscribe((data: any) => {
+        const product = data.recommended_products[0];
+        const productObject = {
+          name: product.name,
+          price: product.price.value,
+          priceFormatted: this.formatPrice(product.price.value, product.price.currency_id),
+          img: product.img_url,
+          link: this.toMlLink(productId)
+        };
+        this.productList.push(productObject);
+        this.updateUrlWithProductIds();
+        this.productLink = '';
+        this.toastr.success('Producto agregado con éxito');
+      });
+    } catch (error) {
+      this.toastr.error('Link invalido! (Estás logeado en ML?)');
+    }
+  }
+
+  formatPrice(value: number, currency: string): string {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(value);
+  }
+
+  toMlLink(productId: string) {
+    return `https://www.mercadolibre.com.ar/p/${productId}`;
+  }
+
+  deleteEntry(index: number) {
+    this.productList.splice(index, 1);
+    this.updateUrlWithProductIds();
+  }
+
+  getTotal() {
+    return this.productList.reduce((acc, product) => acc + product.price, 0);
+  }
+
+  private extractProductId(productLink: string): string {
+    return productLink.split('p/')[1].split('?')[0].split('#')[0];
+  }
+
+  private updateUrlWithProductIds() {
+    const productIds = this.productList.map(product => this.extractProductId(product.link));
+    const url = new URL(window.location.href);
+    url.searchParams.set('products', productIds.join(','));
+    window.history.replaceState({}, '', url.toString());
+  }
+
+  private loadProductsFromUrl() {
+    const url = new URL(window.location.href);
+    const productIds = url.searchParams.get('products');
+    if (productIds) {
+      const idsArray = productIds.split(',');
+      idsArray.forEach(productId => {
+        this.http.get(`${this.apiUrl}&product_id=${productId}`).subscribe((data: any) => {
+          const product = data.recommended_products[0];
+          const productObject = {
+            name: product.name,
+            price: product.price.value,
+            priceFormatted: this.formatPrice(product.price.value, product.price.currency_id),
+            img: product.img_url,
+            link: this.toMlLink(productId)
+          };
+          this.productList.push(productObject);
+        });
+      });
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideToastr(),
+    provideAnimationsAsync(),
+  ]
 };
